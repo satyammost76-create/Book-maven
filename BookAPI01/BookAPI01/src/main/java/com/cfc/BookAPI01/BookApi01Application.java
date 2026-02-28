f1d8e668dad6ba88bc2d7042b65e951df056e121
satyammost76-create <satyammost76@gmail.com>
2026-02-28 09:44:14 +0530

    book-api

---
+++ BookAPI01/BookAPI01/src/main/java/com/cfc/BookAPI01/BookApi01Application.java
@@ -0,0 +1,13 @@
+package com.cfc.BookAPI01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApi01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApi01Application.class, args);
+	}
+
+}
